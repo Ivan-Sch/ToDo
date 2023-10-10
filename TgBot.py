67c560391c314cde8194cccb81e9ca3c87d37d81
Ivan <serbakovv79@gmail.com>
2023-10-10 20:36:21 +0400

    add baza_id with zadachi

--- TgBot.py
+++ TgBot.py
@@ -5,7 +5,7 @@ token = "xx"
 
 bot = telebot.TeleBot(token)
 RANDOM_TASKS = ["Записаться на курс", "Записаться на госуслуги", "Покормить кота", "Помыть машину"]
-my_name = "Иван"
+
 HELP = """
 /help - справка.
 /add - добавить задачу.
@@ -13,17 +13,21 @@ HELP = """
 /random - добавить случайную задачу на дату Сегодня"""
 
 tasks = {}
+baza_id = {}
 
 @bot.message_handler(commands=['start'])
 def say_hi(message):
-    bot.send_message(message.chat.id, f'Привет, {message.chat.first_name}!\n Для получения инструкции напишите /help.')
+    bot.send_message(message.chat.id, f'Привет, {message.chat.id}!\n Для получения инструкции напишите /help.')
 
-def add_todo(date, task):
-    if date in tasks:
-        tasks[date].append(task)
+
+def add_todo(date, task, id):
+    if id not in baza_id:
+        baza_id[id] = {}
+    if date in baza_id[id]:
+        baza_id[id][date].append(task)
     else:
-        tasks[date] = []
-        tasks[date].append(task)
+        baza_id[id][date] = []
+        baza_id[id][date].append(task)
 
 kategory = {"Домашние дела": ["помыть посуду", "приготовить еду"], "Школьные дела": ["сделать уроки", "приготовить рюкзак"]}
 def get_kategory(input):
@@ -43,6 +47,7 @@ def help(message):
     bot.send_message(message.chat.id, HELP)
 
 all_dates = ["сегодня", "завтра"]
+
 @bot.message_handler(commands=["add"])
 def add(message):
     command = message.text.split(maxsplit=2)
@@ -54,15 +59,19 @@ def add(message):
         if len(task) < 3:
             text = "Ошибка- задача меньше трех символов"
         else:
-            add_todo(date, task)
+            add_todo(date, task, message.chat.id)
             text = f"Задача {task} добавлена на дату {date} - {get_kategory(task)}"
     bot.send_message(message.chat.id, text)
+
+@bot.message_handler(commands=["rez"])
+def rez(message):
+    bot.send_message(message.chat.id, str(baza_id))
 
 @bot.message_handler(commands=["random"])
 def random_add(message):
     date = "сегодня"
     task = random.choice(RANDOM_TASKS)
-    add_todo(date, task)
+    add_todo(date, task, message.chat.id)
     text = f"Задача {task} добавлена на дату {date}"
     bot.send_message(message.chat.id, text)
 
@@ -74,9 +83,11 @@ def show(message):
     else:
         date = command[1].lower()
         text = ""
-        if date in tasks:
+        if message.chat.id not in baza_id:
+            baza_id[message.chat.id] = {}
+        if date in baza_id[message.chat.id]:
             text = date.upper() + "\n"
-            for task in tasks[date]:
+            for task in baza_id[message.chat.id][date]:
                 text = f'{text} [] {task} {get_kategory(task)} \n'
         else:
             text = "Задач на эту дату нет"
