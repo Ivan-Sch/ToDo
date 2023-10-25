259e926b2278dcd8d1f8d8f67c1e47cecb825210
Ivan <serbakovv79@gmail.com>
2023-10-25 10:44:00 +0400

    add autodell lastday

--- TgBot.py
+++ TgBot.py
@@ -249,24 +249,13 @@ def del_task(message, date):
 
 
 def update_tasks():
-    # today = datetime.now().strftime('%Y-%m-%d')
     yesterday = (datetime.date.today() - datetime.timedelta(days=1)).strftime('%d.%m.%Y')
-
     # Удаление задач завершенных вчера
     with sqlite3.connect('bazaid.db') as conn:
         cursor = conn.cursor()
         cursor.execute("DELETE FROM bazaid WHERE date=?", (yesterday,))
         conn.commit()
 
-    # # Обновление задач на текущий день
-    # cursor.execute("UPDATE tasks SET date=? WHERE date=?", (today, yesterday))
-    # conn.commit()
-
-# def del_last_date():
-#     last_date = str((datetime.date.today() - datetime.timedelta(days=1)).strftime('%d.%m.%Y'))
-#     for id in baza_id:
-#         if last_date in baza_id[id]:
-#             del baza_id[id][last_date]
 
 # Запускаем планировщик задач
 scheduler = BackgroundScheduler()
